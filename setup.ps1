dad44390a6e958fee83af692d124c4df048929d2
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-08-08 14:57:34 +0900

    [feat] docker-compose 파일 안정화

--- setup.ps1
+++
@@ -1,9 +0,0 @@
-# setup.ps1
-New-Item -ItemType Directory -Path .\app\api -Force
-New-Item -ItemType Directory -Path .\app\services -Force
-New-Item -ItemType Directory -Path .\models -Force
-New-Item -ItemType Directory -Path .\data -Force
-
-New-Item -ItemType File -Path .\app\main.py -Force
-New-Item -ItemType File -Path .\app\api\llama_router.py -Force
-New-Item -ItemType File -Path .\app\services\llama_model.py -Force
